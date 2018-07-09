06f562840fb34c18e736492fc327d799445e2dab
Paolo Oliveira <paolocmo@gmail.com>
2018-07-09 17:32:41 +0200

    add new dependencies for CORS and json middleware

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [compojure "1.5.1"]
                  [com.chain/chain-sdk-java "1.2.1"]
                  [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
                  [ring/ring-jetty-adapter "0.3.8"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler bcpoc.handler/app}
