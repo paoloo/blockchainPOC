3063a53d8e7a8ba378c669cf7f9c21abaa9dfc70
Paolo Oliveira <paolocmo@gmail.com>
2018-09-01 17:33:21 -0300

    add possibility of attaching into a running blockchain

--- src/bcpoc/setup.clj
+++ src/bcpoc/setup.clj
@@ -1,31 +1,35 @@
 (ns bcpoc.setup
   (:import (com.chain.signing HsmSigner)
-          (com.chain.http Client)
-          (com.chain.api MockHsm
-                         MockHsm$Key
-                         Asset
-                         Asset$Builder
-                         Account
-                         Account$Builder)
-          (java.sql Timestamp)))
+		   (com.chain.http Client)
+		   (com.chain.api MockHsm
+						  MockHsm$Key
+						  Asset
+						  Asset$Builder
+						  Account
+						  Account$Builder)
+		   (java.sql Timestamp)))
 
 (defn setup-blockchain
-  [chain-url access-token issuer-alias asset-alias]
-  (def chain (Client. chain-url access-token))
-  (def base-key (MockHsm$Key/create chain (str issuer-alias " KEY")))
-  (HsmSigner/addKey base-key (MockHsm/getSignerClient chain))
-  (->
-    (Asset$Builder.)
-    (.setAlias asset-alias)
-    (.addRootXpub (.xpub base-key))
-    (.setQuorum 1)
-    (.create chain))
-  (def acc  (->
-    (Account$Builder.)
-    (.setAlias issuer-alias)
-    (.addRootXpub (.xpub base-key))
-    (.setQuorum 1)
-    (.create chain)))
-  {:i-key (.xpub base-key) :i-alias issuer-alias :i-id (.id acc) :i-asset asset-alias})
+  ([chain-url access-token issuer-alias asset-alias]
+   (def chain (Client. chain-url access-token))
+   (def base-key (MockHsm$Key/create chain (str issuer-alias " KEY")))
+   (HsmSigner/addKey base-key (MockHsm/getSignerClient chain))
+   (->
+	 (Asset$Builder.)
+	 (.setAlias asset-alias)
+	 (.addRootXpub (.xpub base-key))
+	 (.setQuorum 1)
+	 (.create chain))
+   (def acc  (->
+			   (Account$Builder.)
+			   (.setAlias issuer-alias)
+			   (.addRootXpub (.xpub base-key))
+			   (.setQuorum 1)
+			   (.create chain)))
+   {:i-key (.xpub base-key) :i-alias issuer-alias :i-id (.id acc) :i-asset asset-alias})
+  ([chain-url access-token issuer-alias asset-alias current-i-id current-i-xpub]
+   (if (or (= "" current-i-id) (= "" current-i-xpub))
+     (setup-blockchain chain-url access-token issuer-alias asset-alias)
+     {:i-key current-i-xpub :i-alias issuer-alias :i-id current-i-id :i-asset asset-alias})))
 
 
